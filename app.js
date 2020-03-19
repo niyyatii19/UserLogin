e1cdfc8e88d173d40c53d8b6948780b9d889f077
niyati <niyatig.1998@gmail.com>
2020-03-19 16:25:21 +0530

    Adding files

--- app.js
+++ app.js
@@ -4,12 +4,13 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const mongoose = require("mongoose");
-mongoose.connect("mongodb://localhost:27017/userDB", {
-  useNewUrlParser: true
-});
-const encrypt = require("mongoose-encryption");
-const app = express();
+const session = require("express-session");
+const passport = require("passport");
+const passportLocalMongoose = require("passport-local-mongoose");
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
 
+const app = express();
+const findOrCreate = require('mongoose-findorcreate');
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({
@@ -17,54 +18,124 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(express.static("public"));
 
+app.use(session({
+  secret: "Our Little Secret.",
+  resave: false,
+  saveUninitialized: false
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+mongoose.connect("mongodb://localhost:27017/userDB", {
+  useNewUrlParser: true
+});
+mongoose.set("useCreateIndex", true);
+
 const userSchema = new mongoose.Schema({
   email: String,
-  password: String
+  password: String,
+  googleId: String,
+
 });
 
-
-
-
-userSchema.plugin(encrypt, {secret: process.env.SECRET, encryptedFields: ['password']});
-
+userSchema.plugin(passportLocalMongoose);
+userSchema.plugin(findOrCreate);
 const User= new mongoose.model("User", userSchema);
+
+passport.use(User.createStrategy());
+passport.serializeUser(function(user, done) {
+  done(null, user.id);
+});
+
+passport.deserializeUser(function(id, done) {
+  User.findById(id, function(err, user) {
+    done(err, user);
+  });
+});
+
+passport.use(new GoogleStrategy({
+    clientID: process.env.CLIENT_ID,
+    clientSecret: process.env.CLIENT_SECRET,
+    callbackURL: "http://localhost:3000/auth/google/golog",
+    userProfileURL: "https://www.googleapis.com/oauth2/v3/userinfo"
+  },
+  function(accessToken, refreshToken, profile, cb) {
+    User.findOrCreate({ googleId: profile.id }, function (err, user) {
+      return cb(err, user);
+    });
+  }
+));
+
 
 app.get("/",function(req, res){
   res.render("home");
 });
+
+app.get("/auth/google",
+  passport.authenticate("google",{ scope: ["profile"]})
+);
+
+app.get("/auth/google/golog",
+  passport.authenticate('google', { failureRedirect: '/register' }),
+  function(req, res) {
+    // Successful authentication, redirect home.
+    res.redirect('/golog');
+  });
+
+
 app.get("/login",function(req, res){
   res.render("login");
 });
+
 app.get("/register",function(req, res){
   res.render("register");
 });
 
-app.post("/register",function(req, res){
-  const newUser = new User({
-    email:req.body.username,
-    password: req.body.password
-  });
-  newUser.save(function(err){
-    if(!err){res.render("secrets");
-
-}  });
+app.get("/logout", function (req, res){
+  req.logout();
+  res.redirect("/");
 });
 
-app.post("/login", function (req, res){
-  const username = req.body.username;
-  const password = req.body.password;
-  User.findOne({email: username}, function(err, foundUser){
+app.get("/golog", function(req, res){
+  if(req.isAuthenticated()){
+    res.render("golog");
+  }else{
+    res.redirect("/login");
+  }
+
+});
+
+
+app.post("/register",function(req, res){
+  User.register({username: req.body.username}, req.body.password, function(err, user){
     if(err){
       console.log(err);
+      res.redirect("/register");
     }else{
-      if(foundUser){
-        if(foundUser.password === password){
-          res.render("secrets");
-        }
-      }
+      passport.authenticate("local")(req, res, function(){
+        res.redirect("/login");
+      });
     }
   });
 });
+
+app.post("/login", function (req, res){
+  const user = new User({
+    username: req.body.username,
+    passport: req.body.passport
+  });
+  req.login(user, function(err){
+    if(err){
+      console.log(err);
+    }else{
+      passport.authenticate("local")(req, res, function(){
+        res.redirect("/golog");
+      });
+    }
+  });
+});
+
 app.listen(3000, function() {
   console.log("Server started on port 3000");
 });
